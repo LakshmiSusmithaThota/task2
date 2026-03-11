30713670eb475b5e247d1fbbe3796ca0cd64a155
Lakshmi Susmitha Thota <thotalakshmisusmitha5@gmail.com>
2026-03-11 11:36:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+function togglePassword(id) {
+  let input = document.getElementById(id);
+  input.type = input.type === "password" ? "text" : "password";
+}
+
+// LOGIN VALIDATION
+function loginValidate() {
+  let email = document.getElementById("loginEmail").value;
+  let pass = document.getElementById("loginPassword").value;
+
+  if (email === "" || pass === "") {
+    alert("All fields required");
+    return false;
+  }
+
+  // Dummy AJAX simulation
+  setTimeout(() => {
+    document.getElementById("loginMsg").innerText =
+      "Login successful (Dummy AJAX)";
+  }, 1000);
+
+  return false; // prevent page reload
+}
+
+// REGISTER VALIDATION
+function registerValidate() {
+  let name = document.getElementById("regName").value;
+  let email = document.getElementById("regEmail").value;
+  let pass = document.getElementById("regPassword").value;
+  let confirm = document.getElementById("regConfirm").value;
+
+  if (name === "" || email === "" || pass === "") {
+    alert("All fields required");
+    return false;
+  }
+
+  if (pass.length < 6) {
+    alert("Password must be at least 6 characters");
+    return false;
+  }
+
+  if (pass !== confirm) {
+    alert("Passwords do not match");
+    return false;
+  }
+
+  // Dummy AJAX
+  setTimeout(() => {
+    document.getElementById("registerMsg").innerText =
+      "Registration successful (Dummy AJAX)";
+  }, 1000);
+
+  return false;
+}
